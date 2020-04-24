d9ee048943c72e158423337fa68082c58dd20b05
Paul-Raphael Béliveau Girouard <paulraph6518@gmail.com>
2020-04-23 22:14:02 -0400

    end of section 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
+    CoreModule,
+    ShopModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
