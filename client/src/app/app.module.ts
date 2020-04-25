524f309c1cf0ba27cd25e0c0cbba779f6b2f6420
Paul-Raphael Béliveau Girouard <paulraph6518@gmail.com>
2020-04-24 21:32:31 -0400

    End of section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -14,11 +15,11 @@ import { ShopModule } from './shop/shop.module';
   ],
   imports: [
     CoreModule,
-    ShopModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
